4fb63d1a7c8c9483c44a5cb7df570518320c0b05
Matej1120 <42673835+Matej1120@users.noreply.github.com>
2026-06-20 17:58:56 +0200

    Update vjezba12.php

--- vjezba12.php
+++ vjezba12.php
@@ -63,3 +63,31 @@ if(isset($_POST['naslov']))
 
 </body>
 </html>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Prikaz vijesti</title>
+</head>
+<body>
+
+<h2>Vijest iz kolačića</h2>
+
+<?php
+if(isset($_COOKIE['naslov']) && isset($_COOKIE['tekst']))
+{
+    echo "<h3>" . $_COOKIE['naslov'] . "</h3>";
+    echo "<p>" . $_COOKIE['tekst'] . "</p>";
+}
+else
+{
+    echo "Nema spremljenih podataka.";
+}
+?>
+
+<br>
+<a href="news.php">Dodaj vijest</a>
+
+</body>
+</html>
